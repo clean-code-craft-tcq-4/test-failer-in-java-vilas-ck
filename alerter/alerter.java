4205e63aeb8b0e6667ce95572ec5c9ae9eafacca
CLK1KOR <ck.vilas@bosch.com>
2022-06-23 15:18:15 +0530

    Alter test fixed

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -1,10 +1,14 @@
 public class Alerter {
     static int alertFailureCount = 0;
+    static int threshHold = 200;
     static int networkAlertStub(float celcius) {
         System.out.println("ALERT: Temperature is " + celcius + " celcius");
         // Return 200 for ok
         // Return 500 for not-ok
         // stub always succeeds and returns 200
+        if(celcius>threshHold){
+            return 500;
+        }
         return 200;
     }
     static void alertInCelcius(float farenheit) {
@@ -20,8 +24,21 @@ public class Alerter {
     }
     public static void main(String[] args) {
         alertInCelcius(400.5f);
+        assert(alertFailureCount ==1);
         alertInCelcius(303.6f);
+        assert(alertFailureCount == 1);
+        alertInCelcius(30.5f);
+        assert(alertFailureCount ==1);
+        alertInCelcius(1000.5f);
+        assert(alertFailureCount == 2);
+        alertInCelcius(-100.5f);
+        assert(alertFailureCount ==2);
+        alertInCelcius(105f);
+        assert(alertFailureCount ==2);
+        alertInCelcius(0f);
+        assert(alertFailureCount ==2);
         System.out.printf("%d alerts failed.\n", alertFailureCount);
+        assert(alertFailureCount > 0);
         System.out.println("All is well (maybe!)\n");
     }
 }
