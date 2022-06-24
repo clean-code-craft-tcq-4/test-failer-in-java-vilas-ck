ab30ec626abe025a57befda20806f80f5a54ac09
CLK1KOR <ck.vilas@bosch.com>
2022-06-24 10:08:18 +0530

    misaligned test fixed

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,7 +1,12 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Misaligned {
+
+    static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    static String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
     static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
@@ -10,9 +15,48 @@ public class Misaligned {
         }
         return i * j;
     }
-    public static void main(String[] args) { 
+
+
+    static void printexpectedColorMap() {
+
+        int i = 0, j = 0;
+        for(i = 0; i < 5; i++) {
+            for(j = 0; j < 5; j++) {
+                System.out.printf("%d | %10s | %10s\n", (i * 5 + j)+1, majorColors[i], minorColors[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         int result = printColorMap();
         assert(result == 25);
+
+        // Create a stream to hold the actual output
+        ByteArrayOutputStream actualOutputStream = new ByteArrayOutputStream();
+        PrintStream actualPrintStream = new PrintStream(actualOutputStream);
+
+        // Create a stream to hold the expected output
+        ByteArrayOutputStream expectedOutputStream = new ByteArrayOutputStream();
+        PrintStream expectedPrintStream = new PrintStream(expectedOutputStream);
+
+        PrintStream oldPrintStream  = System.out;
+        System.setOut(actualPrintStream);
+        printColorMap();
+
+        System.out.flush();
+        System.setOut(expectedPrintStream);
+        printexpectedColorMap();
+
+        System.out.flush();
+        System.setOut(oldPrintStream);
+
+        System.out.println("actual: \n" + actualOutputStream.toString());
+        System.out.println("expected: \n" + expectedOutputStream.toString());
+
+        assert(actualOutputStream.toString()==expectedOutputStream.toString());
+
         System.out.println("All is well (maybe!)");
     }
+
+
 }
